6404392901c2906bbf860dcdf08999f90847272d
Nikhil Roy <nikhilchandraroypoet@gmail.com>
2020-02-29 21:15:13 +0600

    Updates

---
+++ precache-manifest.0b2caa3d8c3281b2628d06a899bf62b5.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63831a2d3295c678857a824d4fd48ce6",
+    "url": "/nikhilroy/index.html"
+  },
+  {
+    "revision": "a630411b3ad96c139b60",
+    "url": "/nikhilroy/static/css/main.e4041c75.chunk.css"
+  },
+  {
+    "revision": "478c8f82961947f9eb98",
+    "url": "/nikhilroy/static/js/2.b55f22c1.chunk.js"
+  },
+  {
+    "revision": "f4658cb5fa66ae91b9d4bdca8fe7e6aa",
+    "url": "/nikhilroy/static/js/2.b55f22c1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a630411b3ad96c139b60",
+    "url": "/nikhilroy/static/js/main.5f854a4e.chunk.js"
+  },
+  {
+    "revision": "357961d53fb9d87cb772",
+    "url": "/nikhilroy/static/js/runtime-main.3fe1b918.js"
+  },
+  {
+    "revision": "9fee7af01a6bc4bab54e9656c527ee12",
+    "url": "/nikhilroy/static/media/Project-RJGM.9fee7af0.png"
+  },
+  {
+    "revision": "45bc72e5e5b8b4b9d7be92f206973782",
+    "url": "/nikhilroy/static/media/ReactJS_1.45bc72e5.png"
+  },
+  {
+    "revision": "89ef661c7da5d296df10b23be31740a2",
+    "url": "/nikhilroy/static/media/analog.89ef661c.png"
+  },
+  {
+    "revision": "d34ad3461f785381c0c790b73f5e43a9",
+    "url": "/nikhilroy/static/media/clock_pic.d34ad346.png"
+  },
+  {
+    "revision": "6fad0cd0fd3933441d9c4922274b9d9d",
+    "url": "/nikhilroy/static/media/cuda.6fad0cd0.png"
+  },
+  {
+    "revision": "3cb58c880bf0ea0ac5a91ac51f59204d",
+    "url": "/nikhilroy/static/media/flone.3cb58c88.png"
+  },
+  {
+    "revision": "2d246e6489a4821486ee3a997f5ea03c",
+    "url": "/nikhilroy/static/media/kids.2d246e64.png"
+  },
+  {
+    "revision": "41727997b1d4c87566c33a29b3477aaf",
+    "url": "/nikhilroy/static/media/music_pic.41727997.png"
+  },
+  {
+    "revision": "19520352cf4aaef166a5993a27172fbb",
+    "url": "/nikhilroy/static/media/nikhil.19520352.jpg"
+  },
+  {
+    "revision": "1296a8f042732fa42dca047065f16e9b",
+    "url": "/nikhilroy/static/media/polo.1296a8f0.png"
+  },
+  {
+    "revision": "456e47f23b2b00b45fb4df91ffb6fdce",
+    "url": "/nikhilroy/static/media/projectcrypto.456e47f2.png"
+  },
+  {
+    "revision": "7c1f24fc2296799287df4f7192b903fb",
+    "url": "/nikhilroy/static/media/pujad.7c1f24fc.png"
+  },
+  {
+    "revision": "d9846eb468c5e2c59b9c6589e2648ca4",
+    "url": "/nikhilroy/static/media/vs_icon.d9846eb4.png"
+  }
+]);
