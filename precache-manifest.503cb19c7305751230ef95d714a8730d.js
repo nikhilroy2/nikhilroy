a6348c437211cbfdcf4171113db9b94151c59508
Nikhil Roy <nikhilchandraroypoet@gmail.com>
2020-03-01 07:27:46 +0600

    Updates

---
+++ precache-manifest.503cb19c7305751230ef95d714a8730d.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c246e517070d9e6a2c7aedde0599970",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a622a1f773560df1e3c6",
+    "url": "./static/css/main.e4041c75.chunk.css"
+  },
+  {
+    "revision": "478c8f82961947f9eb98",
+    "url": "./static/js/2.b55f22c1.chunk.js"
+  },
+  {
+    "revision": "f4658cb5fa66ae91b9d4bdca8fe7e6aa",
+    "url": "./static/js/2.b55f22c1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a622a1f773560df1e3c6",
+    "url": "./static/js/main.79e0dd95.chunk.js"
+  },
+  {
+    "revision": "dc905d7b6eef6eeec9c7",
+    "url": "./static/js/runtime-main.d65af7f7.js"
+  },
+  {
+    "revision": "9fee7af01a6bc4bab54e9656c527ee12",
+    "url": "./static/media/Project-RJGM.9fee7af0.png"
+  },
+  {
+    "revision": "45bc72e5e5b8b4b9d7be92f206973782",
+    "url": "./static/media/ReactJS_1.45bc72e5.png"
+  },
+  {
+    "revision": "89ef661c7da5d296df10b23be31740a2",
+    "url": "./static/media/analog.89ef661c.png"
+  },
+  {
+    "revision": "d34ad3461f785381c0c790b73f5e43a9",
+    "url": "./static/media/clock_pic.d34ad346.png"
+  },
+  {
+    "revision": "6fad0cd0fd3933441d9c4922274b9d9d",
+    "url": "./static/media/cuda.6fad0cd0.png"
+  },
+  {
+    "revision": "3cb58c880bf0ea0ac5a91ac51f59204d",
+    "url": "./static/media/flone.3cb58c88.png"
+  },
+  {
+    "revision": "2d246e6489a4821486ee3a997f5ea03c",
+    "url": "./static/media/kids.2d246e64.png"
+  },
+  {
+    "revision": "41727997b1d4c87566c33a29b3477aaf",
+    "url": "./static/media/music_pic.41727997.png"
+  },
+  {
+    "revision": "19520352cf4aaef166a5993a27172fbb",
+    "url": "./static/media/nikhil.19520352.jpg"
+  },
+  {
+    "revision": "1296a8f042732fa42dca047065f16e9b",
+    "url": "./static/media/polo.1296a8f0.png"
+  },
+  {
+    "revision": "456e47f23b2b00b45fb4df91ffb6fdce",
+    "url": "./static/media/projectcrypto.456e47f2.png"
+  },
+  {
+    "revision": "7c1f24fc2296799287df4f7192b903fb",
+    "url": "./static/media/pujad.7c1f24fc.png"
+  },
+  {
+    "revision": "d9846eb468c5e2c59b9c6589e2648ca4",
+    "url": "./static/media/vs_icon.d9846eb4.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/nikhilroy/precache-manifest.0b2caa3d8c3281b2628d06a899bf62b5.js"
+  "./precache-manifest.503cb19c7305751230ef95d714a8730d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nikhilroy/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
